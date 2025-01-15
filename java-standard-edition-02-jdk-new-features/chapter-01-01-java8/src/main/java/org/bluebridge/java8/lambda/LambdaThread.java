1c8c0606afe5cc21edf482e58c3c19e61fff652b
lingwh <lingwh.walkman@homail.com>
2025-01-15 22:39:51 +0800

    修改项目结构

--- java-standard-edition-02-jdk-new-features/chapter-01-01-java8/src/main/java/org/bluebridge/java8/lambda/LambdaThread.java
+++ java-standard-edition-02-jdk-new-features/chapter-01-01-java8/src/main/java/org/bluebridge/java8/lambda/LambdaThread.java
@@ -4,8 +4,29 @@ package org.bluebridge.java8.lambda;
  * @author ronin
  */
 public class LambdaThread {
-    public static void main(String[] args) {
-        new Thread(()-> System.out.println("Thread1 is running......")).start();
-        new Thread(()-> {System.out.println("Thread2 is running......");}).start();
-    }
+    
+	@Test
+	public void testLambdaHelloWorld() {
+    	/**
+    	 * 不使用lambda函数创建线程
+    	 */
+    	new Thread(new Runnable() {
+    		@Override
+    		public void run() {
+    			System.out.println("Thread1 is running......");
+    		}
+    	}).start();
+    	
+    	/**
+    	 * 使用lambda函数创建线程
+    	 */
+    	new Thread(() -> {
+    		System.out.println("Thread2 is running......");
+    	}).start();
+    	
+    	/**
+    	 * 使用lambda函数创建线程(简写形式)
+    	 */
+    	new Thread(() -> System.out.println("Thread2 is running......")).start();
+	}
 }
