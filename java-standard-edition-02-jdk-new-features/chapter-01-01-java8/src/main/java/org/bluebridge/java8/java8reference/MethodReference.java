13aaa00dd811321f6102b228a31b6113baf5aebe
think <lingwh.walkman@hotmail.com>
2025-01-21 14:22:54 +0800

    修改项目结构

--- java-standard-edition-02-jdk-new-features/chapter-01-01-java8/src/main/java/org/bluebridge/java8/java8reference/MethodReference.java
+++ java-standard-edition-02-jdk-new-features/chapter-01-01-java8/src/main/java/org/bluebridge/java8/java8reference/MethodReference.java
@@ -308,7 +308,38 @@ public class MethodReference {
 	 * 			案例五: Comparator<String> comparator = String :: compareTo;
 	 */
 	@Test
-	public void testMethodReference5() {
+	public void testInstanceReference5() {
+		//方法引用第三种形式的第二个例子
+		List<String> cities = Arrays.asList("shanghai", "beijing", "tianjin");
+
+		//匿名内部类
+		Collections.sort(cities, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		});
+		cities.forEach(item -> System.out.println(item));
+		System.out.println("------------------------------");
+
+		//lambda表达式
+		Collections.sort(cities,(city1,city2) -> city1.compareTo(city2));
+		cities.forEach(item -> System.out.println(item));
+		System.out.println("------------------------------");
+
+		//方法引用
+		Collections.sort(cities,String::compareTo);
+		cities.forEach(item -> System.out.println(item));
+		System.out.println("------------------------------");
+	}
+
+
+	/**
+	 * 情况二: 	实例方法引用(instanceReference::instanceMethodName)
+	 * 			案例六: Comparator<String> comparator = String :: compareTo;
+	 */
+	@Test
+	public void testMethodReference6() {
 		//匿名内部类
 		BiPredicate<String, String> biPredicate = new BiPredicate<String,String>() {
 			@Override
@@ -332,10 +363,10 @@ public class MethodReference {
 
 	/**
 	 * 情况二: 	实例方法引用(类名::instanceMethodName)
-	 * 			案例流: Function<Person, String> function = Person :: getUsername;
+	 * 			案例七: Function<Person, String> function = Person :: getUsername;
 	 */
 	@Test
-	public void testMethodReference6() {
+	public void testMethodReference7() {
 		Person person = new Person("张三",128);
 		//匿名内部类
 		Function<Person, String> function = new Function<Person,String>() {
@@ -360,6 +391,7 @@ public class MethodReference {
 		System.out.println("------------------------------");
 	}
 
+
 	/**
 	 * 情况三: 	构造方法引用(类名::new)
 	 * 			案例一: ArrayList::new
@@ -367,6 +399,17 @@ public class MethodReference {
 	@Test
 	public void testConstructorReference1(){
 		Supplier<ArrayList<Integer>> supplier = ArrayList :: new;
+		System.out.println(supplier.get());
+	}
+
+
+	/**
+	 * 情况三: 	构造方法引用(类名::new)
+	 * 			案例二: Person::new
+	 */
+	@Test
+	public void testConstructorReference2(){
+		Supplier<Person> supplier = Person :: new;
 		System.out.println(supplier.get());
 	}
 
@@ -430,94 +473,7 @@ public class MethodReference {
 		System.out.println(Arrays.toString(arr2));
 	}
 
-	//https://blog.csdn.net/a_beiyo/article/details/142216688
-    public static void main(String[] args) {
-//
-//        List<String> list = Arrays.asList("a", "b", "c", "d", "e");
-//        list.forEach(item -> System.out.println(item));
-//        System.out.println("------------------------------------");
-//        list.forEach(System.out::println);
-//
-//        System.out.println("------------------------------------");
-//        //排序
-//        //lambda写法
-//        ArrayList<Person> persons = new ArrayList<>();
-//        persons.add(new Person("ls",28));
-//        persons.add(new Person("zs",18));
-//        persons.add(new Person("ww",48));
-//        persons.add(new Person("ls",38));
-//        //persons.sort((p1,p2) -> Person.compare(p1,p2));
-//        persons.forEach(item -> System.out.println(item));
-//        System.out.println("------------------------------------");
-//        //方法引用写法
-//        //persons.sort(PersonComparator::compareStatic);
-//        persons.forEach(item -> System.out.println(item));
-//
-//        System.out.println("------------------------------------");
-//        /**
-//         * 2.实例方法引用:方法参数由编译器自动识别
-//         *      注意:被引用的方法不能是静态的,会报错
-//         */
-//        PersonComparator personComparator = new PersonComparator();
-//        //persons.sort((p1,p2) -> personComparator.compareNoStatic(p1,p2));
-//        //persons.sort(personComparator::compareNoStatic);
-//        persons.forEach(item -> System.out.println(item));
-//        System.out.println("------------------------------------");
-//
-//        /**
-//         * 3.类名::实例方法名
-//         *      方法的调用者是lambda表达式的第一个参数,第一个参数就是调用者，其他参数作为方法参数
-//         *      代表person.getAge() - this.getAge()中的this
-//         */
-//        persons.sort(Person::compare);
-//        persons.forEach(item -> System.out.println(item));
-//        System.out.println("------------------------------------");
-//
-//        //方法引用第三种形式的第二个例子
-//        List<String> cities = Arrays.asList("shanghai", "beijing", "tianjin");
-//        //lambda表达式形式
-////        Collections.sort(cities,(city1,city2) -> city1.compareTo(city2));
-//        //使用方法引用代替lambda表达式
-//        Collections.sort(cities,String::compareTo);
-//        cities.forEach(item -> System.out.println(item));
-//
-//        System.out.println("------------------------------------");
-//        /**
-//         * 4.构造方法引用
-//         */
-//        System.out.println(getString(String::new));
-//        System.out.println(getString("xx",String::new));
-//        Supplier<Person> person = Person::new;
-//        System.out.println(person);
-//
-//        System.out.println("------------------------------------");
-//        /**
-//         * 测试只能使用方法引用,不能使用lambda表达式
-//         */
-//        List<Person> personList = Arrays.asList(new Person[]{new Person("zs",18),new Person("ls",28),new Person("ww",38)});
-//        personList.forEach(System.out::println);
-//        personList.forEach(p -> System.out.println(p.getUsername()));
 
-    }
-
-    /**
-     * 测试构造方法引用1
-     * @param supplier
-     * @return
-     */
-    public static String getString(Supplier<String> supplier){
-        return supplier.get() + "test";
-    }
-
-    /**
-     * 测试构造方法引用2
-     * @param string
-     * @param function
-     * @return
-     */
-    public static String getString(String string,Function<String,String> function){
-        return function.apply(string) + "test";
-    }
 }
 
 class Person {
