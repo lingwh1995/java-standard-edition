60ec4ecd0352dd07fc7fa5edcb98aa3ddc5b75e0
think <lingwh.walkman@hotmail.com>
2025-02-17 16:22:54 +0800

    修改项目结构

---
+++ java-standard-edition-01-jdk-docs/chapter-01-01-basic/src/main/java/org.bluebridge.beginner/MultiConditionJudgeTest.java
@@ -0,0 +1,25 @@
+package org.bluebridge.beginner;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 多条件判断简化写法
+ */
+public class MultiConditionJudgeTest {
+
+    @Test
+    public void test() {
+        //普通写法
+        String food = "banana";
+        if(food.equals("banana") || food.equals("apple") || food.equals("orange")) {
+            System.out.println("食物的种类是水果......");
+        }
+
+        //优化写法
+        if(Arrays.asList("banana","apple","orange").contains(food)) {
+            System.out.println("食物的种类是水果......");
+        }
+    }
+}
