82229e344fc021ffa52c9d55d93b86217a24890d
lingwh <lingwh.walkman@homail.com>
2025-03-19 22:32:20 +0800

    修改项目结构

---
+++ java-standard-edition-03-jdk-concurrency/chapter-08-demo/src/main/java/org/bluebridge/SimpleDateTest.java
@@ -0,0 +1,43 @@
+package org.bluebridge;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateTest {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        // 测试多线程情况下 sdf.format()
+        /*
+        for (int i=0; i<100; i++) {
+            new Thread(() -> {
+                System.out.println("format = " + sdf.format(new Date()));
+            }).start();
+        }*/
+
+        // 测试多线程情况下 sdf.parse()
+        /*
+        for (int i=0; i<100; i++) {
+            new Thread(() -> {
+                try {
+                    System.out.println("parse = " + sdf.parse("1995-09-17"));
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+        }*/
+
+        //使用 synchronized 解决线程安全问题
+        for (int i=0; i<100; i++) {
+            new Thread(() -> {
+                synchronized (sdf) {
+                    try {
+                        System.out.println("parse = " + sdf.parse("1995-09-17"));
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }).start();
+        }
+    }
+}
