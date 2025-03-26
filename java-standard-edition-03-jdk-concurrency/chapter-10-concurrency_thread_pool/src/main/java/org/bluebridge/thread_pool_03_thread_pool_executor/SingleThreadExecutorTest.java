a477bdd0f65ca7589f5438b9af5aadc3669f2d6c
think <lingwh.walkman@hotmail.com>
2025-03-26 17:15:55 +0800

    修改项目结构

---
+++ java-standard-edition-03-jdk-concurrency/chapter-10-concurrency_thread_pool/src/main/java/org/bluebridge/thread_pool_03_thread_pool_executor/SingleThreadExecutorTest.java
@@ -0,0 +1,21 @@
+package org.bluebridge.thread_pool_03_thread_pool_executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingleThreadExecutorTest {
+    public static void main(String[] args) {
+        // 创建一个固定大小为4的线程池
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        // 提交任务
+        for (int i = 0; i < 10; i++) {
+            executor.execute(() -> {
+                System.out.println("Task executed by " + Thread.currentThread().getName());
+            });
+        }
+
+        // 关闭线程池（必须显式关闭）
+        executor.shutdown();
+    }
+}
