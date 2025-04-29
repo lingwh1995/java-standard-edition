9742a78e9edb24e3f490900d4c11177f3d681713
lingwh <lingwh.walkman@homail.com>
2025-04-29 21:39:30 +0800

    修改项目结构

--- java-standard-edition-05-jdk-libs/chapter-03-iotutils/src/main/java/org/bluebridge/utils/PaddingUtilTest.java
+++ java-standard-edition-05-jdk-libs/chapter-03-iotutils/src/main/java/org/bluebridge/utils/PaddingUtilTest.java
@@ -3,6 +3,7 @@ package org.bluebridge.utils;
 import cn.hutool.core.util.HexUtil;
 
 public class PaddingUtilTest {
+
     public static void main(String[] args) {
         byte[] dataAreaBytes = HexUtil.decodeHex("00112233445566778899AABBCCDDEEFF");
         System.out.println("数组长度:" + dataAreaBytes.length);
