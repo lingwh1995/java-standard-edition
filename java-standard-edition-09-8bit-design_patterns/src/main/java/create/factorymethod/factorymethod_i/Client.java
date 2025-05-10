a59616667070ce68c56353433feb3bff796c3b92
think <lingwh.walkman@hotmail.com>
2025-05-10 15:36:49 +0800

    修改项目结构

--- java-standard-edition-09-8bit-design_patterns/src/main/java/create/factorymethod/factorymethod_i/Client.java
+++ java-standard-edition-09-8bit-design_patterns/src/main/java/create/factorymethod/factorymethod_i/Client.java
@@ -1,7 +1,5 @@
 package create.factorymethod.factorymethod_i;
 
-import org.junit.Test;
-
 public class Client {
 	
 	@Test
---
+++ java-standard-edition-10-8bit-senior/chapter-01-fsm/src/main/java/org/bluebridge/fsm/FSMTest.java
@@ -0,0 +1,46 @@
+package org.bluebridge.fsm;
+
+public class FSMTest {
+    /**
+     * 状态码
+     */
+    private static final int BEGIN = 0;
+    private static final int IN_WORD = 1;
+    private static final int OUT_WORD = 2;
+    private static final int END = 3;
+
+    public static void main(String[] args) {
+        String s = " one  two   three    four     five ";
+        int wordCount = 0;
+        int state = BEGIN;
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            switch (state){
+                case BEGIN:
+                    if(Character.isLetter(c)){
+                        state = IN_WORD;
+                    }else {
+                        state = OUT_WORD;
+                    }
+                    break;
+                case IN_WORD:
+                    if(!Character.isLetter(c)){
+                        wordCount++;
+                        state = OUT_WORD;
+                    }
+                    break;
+                case OUT_WORD:
+                    if(Character.isLetter(c)){
+                        state = IN_WORD;
+                    }
+                    break;
+            }
+        }
+        if(state == IN_WORD) {
+            wordCount++;
+        }
+        state = END;
+        System.out.println("单词数量 = " + wordCount);
+        System.out.println("状态机状态 = " + state);
+    }
+}
