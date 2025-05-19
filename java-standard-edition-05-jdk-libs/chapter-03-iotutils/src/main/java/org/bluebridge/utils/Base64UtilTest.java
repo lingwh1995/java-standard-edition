ba4fda5e74bd69784717a034405f307e777cd5a5
think <lingwh.walkman@hotmail.com>
2025-05-19 18:50:53 +0800

    修改项目结构

--- java-standard-edition-05-jdk-libs/chapter-03-iotutils/src/main/java/org/bluebridge/utils/Base64UtilTest.java
+++ java-standard-edition-05-jdk-libs/chapter-03-iotutils/src/main/java/org/bluebridge/utils/Base64UtilTest.java
@@ -5,28 +5,11 @@ import org.bluebridge.utils.utils.Base64Util;
 public class Base64UtilTest {
     public static void main(String[] args) {
         //IMEI 867860061213483
-        String stringMessage = "68671500610001900125051516264039393939393939393939393939393939000138363139383730373138353439343338393836313132343231303030333237303931300000424339352D434E56000000000846177709CA00A0FFA200E39B7616";
+        String stringMessage = "680001010C01413005B3821379F3F04EBBEF53D22BFE5EBF202492DD2ECD6FB2A6C9B90F503AF38C8D92A2D4F4FD554C08E596C3535471F8E39273FF9217BD2BD518BEF2CC0F68D3042492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8D2492DD2ECD6FB2A6C9B90F503AF38C8DA2EDBCEE476D8395DF20F252780DFB5CF91F16";
         String base64Message = Base64Util.stringToBase64(stringMessage);
         System.out.println("base64Message = " + base64Message);
 
         stringMessage = Base64Util.base64ToString(base64Message);
         System.out.println("stringMessage = " + stringMessage);
-
-        // 如果不相等，则用大数减去小数，然后用这个较小数与它们相减的结果相比较，如果相等，则这个差就是它们的最大公约数，而如果不相等，则继续执行②操作。
-        int a = 100, b = 80, diff = 100;
-        while(diff >= 0) {
-            if(a < b)
-            {
-                int temp = a;
-                a = b;
-                b = temp;
-            }
-            diff = a - b;
-            a = b;
-            b = diff;
-        }
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(diff);
     }
 }
