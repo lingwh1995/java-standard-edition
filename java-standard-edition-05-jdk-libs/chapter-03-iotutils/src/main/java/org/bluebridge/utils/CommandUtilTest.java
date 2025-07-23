bb105469b5ec9fed8febf5598d2870602ec00eb9
think <lingwh.walkman@hotmail.com>
2025-07-23 09:32:20 +0800

    修改项目结构

--- java-standard-edition-05-jdk-libs/chapter-03-iotutils/src/main/java/org/bluebridge/utils/CommandUtilTest.java
+++ java-standard-edition-05-jdk-libs/chapter-03-iotutils/src/main/java/org/bluebridge/utils/CommandUtilTest.java
@@ -12,13 +12,13 @@ public class CommandUtilTest {
     @Test
     public void testBuildFinalDataArea() throws Exception {
         // 主密钥
-        String mainSecret = "31323334353637383930313233343536";
+        String mainSecret = "7A89EF7C62C83D154F25E31E121250CC";
         // 随机通信码
-        String randomCode = "20250430999999999999999900000000";
+        String randomCode = "4D391818394D15F50D2D86862D0D467A";
         // 数据区
-        String dataAreaHex = "01";
+        String dataAreaHex = "0000250718003224";
         // 是否加密数据区
-        boolean isEncryp = true;
+        boolean isEncryp = false;
         // 是否计算mac并添加mac到数据区尾部
         boolean withMac = true;
         CommandUtil commandUtil = new CommandUtil(mainSecret, randomCode, dataAreaHex, isEncryp, withMac);
@@ -35,7 +35,7 @@ public class CommandUtilTest {
         // 主密钥
         String mainSecret = "31323334353637383930313233343536";
         // 随机通信码
-        String randomCode = "20250508999999999999999900000000";
+        String randomCode = "4D391818394D15F50D2D86862D0D467A";
         // 数据区
         // 6868150015282499999999999999990009 01 21b9 16
         //68680a009407019999999999999996910508be9cb04fec345f9ec2ced957691faab7330fb7c95782fc3d67e7c3a66728dab17c939713873ebb1def93b174f9bc4a07a39c4f1fb2866625ed3c1bd31fc05eae072ffa666731d1522212f0f2911072495d02924e94345cf5b6af8b81e81b0a770c8a0a2235e771b7ff80e3a418d9ee4fd4575d86aecb8d34694da27d4834cb68b116
--- java-standard-edition-05-jdk-libs/chapter-03-iotutils/src/main/java/org/bluebridge/utils/utils/CRCUtil.java
+++ java-standard-edition-05-jdk-libs/chapter-03-iotutils/src/main/java/org/bluebridge/utils/utils/CRCUtil.java
@@ -1,13 +1,36 @@
 package org.bluebridge.utils.utils;
 
-import cn.hutool.core.io.checksum.crc16.CRC16XModem;
+import cn.hutool.core.io.checksum.crc16.*;
 
 /**
  * HuTools提供了常见的CRC实现 根据需要调用即可
- *
- * @author lenovo
  */
 public class CRCUtil {
+
+    public static long CCITT(byte[] bytes) {
+        CRC16CCITT crc16CCITT = new CRC16CCITT();
+        crc16CCITT.update(bytes);
+        return crc16CCITT.getValue();
+    }
+
+    //  可指定数组长度
+    public static long CCITT(byte[] bytes, int start, int end) {
+        CRC16CCITT crc16CCITT = new CRC16CCITT();
+        crc16CCITT.update(bytes, start, end);
+        return crc16CCITT.getValue();
+    }
+
+    public static long CCITTFalse(byte[] bytes) {
+        CRC16CCITTFalse crc16CCITTFalse = new CRC16CCITTFalse();
+        crc16CCITTFalse.update(bytes);
+        return crc16CCITTFalse.getValue();
+    }
+
+    public static long crc16Modbus(byte[] bytes) {
+        CRC16Modbus crc16Modbus = new CRC16Modbus();
+        crc16Modbus.update(bytes);
+        return crc16Modbus.getValue();
+    }
 
     public static long XModem(byte[] bytes) {
         CRC16XModem crc16XModem = new CRC16XModem();
@@ -15,4 +38,92 @@ public class CRCUtil {
         return crc16XModem.getValue();
     }
 
+    public static long crcIbm(byte[] bytes) {
+        CRC16IBM crc16IBM = new CRC16IBM();
+        crc16IBM.update(bytes);
+        return crc16IBM.getValue();
+    }
+
+    public static long crcUsb(byte[] bytes) {
+        CRC16USB crc16USB = new CRC16USB();
+        crc16USB.update(bytes);
+        return crc16USB.getValue();
+    }
+
+    public static long crcX25(byte[] bytes) {
+        CRC16X25 crc16X25 = new CRC16X25();
+        crc16X25.update(bytes);
+        return crc16X25.getValue();
+    }
+
+    /**
+     * 调用示例
+     */
+    public static void main(String[] args) {
+
+        String hex = "68260030010001999999999999250719000000000d0000009999999999990004a0b598";
+        // 测试数据
+        byte[] testBytes = ParseUtil.hexStringToByte(hex);
+
+        // CCITT
+        CRC16CCITT crc16CCITT = new CRC16CCITT();
+        // 可以指定数组起始位置和长度
+        crc16CCITT.update(testBytes, 0, testBytes.length);
+        String ccitt = crc16CCITT.getHexValue();
+        System.out.println("CCITT = " + ccitt);
+
+        // CCITTFalse
+        CRC16CCITTFalse crc16CCITTFalse = new CRC16CCITTFalse();
+        crc16CCITTFalse.update(testBytes);
+        String ccittFalseHexValue = crc16CCITTFalse.getHexValue();
+        System.out.println("ccittFalseHexValue = " + ccittFalseHexValue);
+
+        // Modbus
+        CRC16Modbus crc16Modbus = new CRC16Modbus();
+        crc16Modbus.update(testBytes);
+        String modbusHexValue = crc16Modbus.getHexValue();
+        System.out.println("modbusHexValue = " + modbusHexValue);
+
+        // XModem
+        CRC16XModem crc16XModem = new CRC16XModem();
+        crc16XModem.update(testBytes);
+        String xModemHexValue = crc16XModem.getHexValue();
+        System.out.println("xModemHexValue = " + xModemHexValue);
+
+        // IBM
+        CRC16IBM crc16IBM = new CRC16IBM();
+        crc16IBM.update(testBytes);
+        String ibmHexValue = crc16IBM.getHexValue();
+        System.out.println("ibmHexValue = " + ibmHexValue);
+
+        // USB
+        CRC16USB crc16USB = new CRC16USB();
+        crc16USB.update(testBytes);
+        String usbHexValue = crc16USB.getHexValue();
+        System.out.println("usbHexValue = " + usbHexValue);
+
+        // X25
+        CRC16X25 crc16X25 = new CRC16X25();
+        crc16X25.update(testBytes);
+        String x25HexValue = crc16X25.getHexValue();
+        System.out.println("x25HexValue = " + x25HexValue);
+
+        // CRC16Ansi
+        CRC16Ansi crc16Ansi = new CRC16Ansi();
+        crc16Ansi.update(testBytes);
+        String crc16AnsiHexValue = crc16Ansi.getHexValue();
+        System.out.println("crc16AnsiHexValue = " + crc16AnsiHexValue);
+
+        // CRC16DNP
+        CRC16DNP crc16DNP = new CRC16DNP();
+        crc16DNP.update(testBytes);
+        String crc16DNPHexValue = crc16DNP.getHexValue();
+        System.out.println("crc16DNPHexValue = " + crc16DNPHexValue);
+
+        // CRC16Maxim
+        CRC16Maxim crc16Maxim = new CRC16Maxim();
+        crc16Maxim.update(testBytes);
+        String crc16MaximHexValue = crc16Maxim.getHexValue();
+        System.out.println("crc16MaximHexValue = " + crc16MaximHexValue);
+    }
 }
