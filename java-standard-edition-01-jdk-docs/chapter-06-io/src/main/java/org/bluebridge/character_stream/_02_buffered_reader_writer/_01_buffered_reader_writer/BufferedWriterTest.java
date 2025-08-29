80a5bafbfe2f6679680b0d7216639ea3ab9a2216
think <lingwh.walkman@hotmail.com>
2025-08-29 11:41:30 +0800

    修改项目结构

--- java-standard-edition-01-jdk-docs/chapter-06-io/src/main/java/org/bluebridge/character_stream/_02_buffered_reader_writer/_01_buffered_reader_writer/BufferedWriterTest.java
+++ java-standard-edition-01-jdk-docs/chapter-06-io/src/main/java/org/bluebridge/character_stream/_02_buffered_reader_writer/_01_buffered_reader_writer/BufferedWriterTest.java
@@ -6,35 +6,45 @@ package org.bluebridge.character_stream._02_buffered_reader_writer._01_buffered_
  * @date 2025/8/29 9:54
  */
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 /**
  * BufferedWriter主要特点
- *    缓冲机制: 内部使用缓冲区减少实际的写出操作次数
- *    行读取支持: 提供高效的按行写出方法
- *    性能优化: 比直接写出字符流更高效
+ *    缓冲机制: 内部使用缓冲区减少实际的写入操作次数
+ *    行读取支持: 提供高效的按行写入方法
+ *    性能优化: 比直接写入字符流更高效
  *    字符流操作: 专门用于处理字符数据而非字节数据
  * 最佳实践
  *    始终关闭资源: 使用 try-with-resources 或确保在 finally 块中关闭 BufferedWriter
  *    合适的缓冲区大小: 根据数据特征选择合适的缓冲区大小，默认通常是8192字符
- *    优先使用 writeLine(): 处理文本行时优先使用 writeLine() 而非逐字符写出
+ *    优先使用 newLine() 方法写入平台相关的行分隔符，而不是在写入时使用手动拼接的换行符，如 "\n" 或 "\r\n"
  *    异常处理: 妥善处理 IOException 异常、
  *
- * BufferedWriter 是处理文本数据时非常重要的类，通过缓冲机制显著提高了文本写出的性能，特别是在处理大量文本数据时效果明显。
+ * BufferedWriter 是处理文本数据时非常重要的类，通过缓冲机制显著提高了文本写入的性能，特别是在处理大量文本数据时效果明显。
  */
+@Slf4j(topic = "·")
 public class BufferedWriterTest {
 
+    /**
+     * 测试BufferedWriter
+     */
+    @Test
     public void testBufferedWriter() {
-        // 使用try-with-resources语法，自动关闭资源
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("buffered_writer.txt"))) {
             writer.write("Hello, BufferedWriter!");
             writer.newLine();
             writer.write("这是第二行内容");
-            System.out.println("数据写入完成");
+            // 确保数据被写入
+            writer.flush();
+            log.info("数据写入完成");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
