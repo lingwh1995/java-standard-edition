28b8534672975ed28f9a0b8810d35d05c4b1509f
lingwh <lingwh.walkman@homail.com>
2025-09-22 21:20:33 +0800

    修改项目项目结构

---
+++ java-standard-edition-01-jdk-docs/chapter-01-basic/src/main/java/org/bluebridge/beginner/DataTypeConversionTest.java
@@ -0,0 +1,22 @@
+package org.bluebridge.beginner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j(topic = "·")
+public class DataTypeConversionTest {
+
+    @Test
+    public void testDataTypeConversion() {
+        // 原始数据类型
+        int i = 100;
+        log.info("i = {}", i);
+        // 小范围数据类型 => 大范围数据类型（不需要进行强制类型转换）
+        double d = i;
+        log.info("d = {}", d);
+        // 大范围数据类型 => 小范围数据类型（需要进行强制类型转换）
+        char c = (char) i;
+        log.info("c = {}", c);
+    }
+
+}
