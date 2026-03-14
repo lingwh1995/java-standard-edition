f091be3851a48cdc8795f126d4556590c781f777
lingwh <lingwh.walkman@homail.com>
2026-03-14 15:12:07 +0800

    修改项目结构

---
+++ java-standard-edition-01-jdk-docs/chapter-01-basic/src/main/java/org/bluebridge/basic/IntegerTest.java
@@ -0,0 +1,21 @@
+package org.bluebridge.basic;
+
+import org.junit.Test;
+
+/**
+ * @author lingwh
+ * @desc
+ * @date 2026/3/14 14:19
+ */
+public class IntegerTest {
+
+    /**
+     * 统计二进制位中1的个数
+     */
+    @Test
+    public void bitCountTest() {
+        int i = 8;
+        System.out.println(Integer.bitCount(i));
+    }
+
+}
